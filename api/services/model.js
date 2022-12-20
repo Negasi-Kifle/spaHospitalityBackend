5c25812b19fd31f5242fb695b074ca02357024da
Negasi <negasi@Negasis-MacBook-Air.local>
2022-12-20 23:38:42 +0300

    Service model added

---
+++ api/services/model.js
@@ -0,0 +1,47 @@
+/**
+ * This file is for services model
+ */
+
+// Mongoose
+const mongoose = require("mongoose");
+
+// Services model
+const servicesSchema = new mongoose.Schema(
+  {
+    serviceName: {
+      type: String,
+      required: [true, "Service name is required"],
+    },
+    serviceDuration: Number,
+    priceBeforeVAT: {
+      type: Number,
+      required: [true, "Service price before VAT is required"],
+    },
+    serviceVAT: Number,
+    priceAfterVAT: {
+      type: Number,
+      required: [true, "Service price after VAT is required"],
+    },
+    commission: Number,
+    description: String,
+  },
+  {
+    writeConcern: {
+      w: "majority",
+      j: true,
+    },
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+// Model
+const Services = mongoose.model("Services", servicesSchema);
+
+// Export model
+module.exports = Services;
